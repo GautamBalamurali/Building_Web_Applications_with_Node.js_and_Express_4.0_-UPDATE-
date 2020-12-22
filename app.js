3be72fc25a9e55f168d4079967a363f099229863
unknown <gautam.b@gadgeon.com>
2020-12-22 22:16:53 +0530

    Setting up tooling

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname,'/public')));
-app.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
-app.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
-app.use('/js',express.static(path.join(__dirname,'/node_modules/jquery/dist')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'views/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(1600,() => {
-    debug(`Listening on ${chalk.green('1600...')}`);
+app.listen(port, () => {
+  debug(`Listening on ${chalk.green(`${port}...`)}`);
 });
